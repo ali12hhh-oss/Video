5830ee444f7c139a8dea82a2792769899341422e
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-19 00:36:27 +0300

    Fix Kotlin opt-in annotation for editor build

--- app/src/main/java/com/videoforge/nativeeditor/MainActivity.kt
+++ app/src/main/java/com/videoforge/nativeeditor/MainActivity.kt
@@ -1,4 +1,4 @@
-@file:androidx.annotation.OptIn(markerClass = [androidx.media3.common.util.UnstableApi::class, androidx.compose.material3.ExperimentalMaterial3Api::class])
+@file:OptIn(\n    androidx.media3.common.util.UnstableApi::class,\n    androidx.compose.material3.ExperimentalMaterial3Api::class\n)
 
 package com.videoforge.nativeeditor
 
